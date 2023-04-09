c754101e167150d7cdb073f0a81e2f55d7d01378
patel_nishant <patel.nishant@northeastern.edu>
2023-04-09 13:34:19 -0400

    Structure for Assignment 10

---
+++ weather-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
+
+function App() {
+  return (
+    <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400 rounded-lg'>
+      
+    </div>
+  );
+}
+
+export default App;
